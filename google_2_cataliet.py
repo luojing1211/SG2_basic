abcb49a51add7fe17559a2e5eb5bca76aec55d6b
jingluo <jingluo@jingluos-MacBook-Air.local>
2017-08-03 22:31:52 -0500

    add catalitefile class

--- google_2_cataliet.py
+++ google_2_cataliet.py
@@ -13,7 +13,7 @@ import numpy as np
 class SG2Image(object):
     """This is a class for sg2 image information.
     """
-    def __init__(self, image_full_id, **infos):
+    def __init__(self, image_full_id=None):
         """
         Parameter
         ---------
@@ -22,13 +22,18 @@ class SG2Image(object):
         **infos: dict
             Input information.
         """
+        if image_full_id != None:
+            self.set_id(image_full_id)
+
+    def set_id(self, image_full_id):
         self.image_full_id = image_full_id
-        self.input_info = infos
         id_split = self.image_full_id.split('-')
         self.mission_id = id_split[0]
         self.separation_sign = id_split[1]
         self.image_id = id_split[2]
-        for key, v in list(self.input_info.items()):
+
+    def add_info(self, info):
+        for key, v in list(info.items()):
             setattr(self, key, v)
 
     def get_camera_info(self, camera_file_contents=None, camera_dir=None):
@@ -58,15 +63,64 @@ class GoogleSheetCSV(object):
         self.filename = csv_file
         self.read_csv_file(self.filename)
         self.content_list, self.name_field = self.read_csv_file(self.filename)
-        
+        self.image_ids = self.content_list[:, 1]
+        self.missions, self.missions_list = self.get_missions(self.image_ids)
+
     def read_csv_file(self, csv_file):
-        name_field = []
+        name_field = {}
         with open(csv_file, 'r') as f:
              reader = csv.reader(f)
              content_list = list(reader)
         for line in content_list:
             if line[0] == 'ID':
-                for l in line:
-                    name_field.append(l)
+                for ii, l in enumerate(line):
+                    if l == '':
+                        continue
+                    name_field[l] = ii
                 break
         return np.array(content_list), name_field
+
+    def get_missions(self, image_ids):
+        missions_list = []
+        for image_id in image_ids:
+            try:
+                image_id_fields = image_id.split('-')
+                missions_list.append(image_id_fields[0])
+            except:
+                missions_list.append('')
+        missions = set(missions_list)
+        missions.remove('')
+        missions.remove('Image ID')
+        return missions, np.array(missions_list)
+
+class CataliteFiles(object):
+    """This is a class for catelite output file.
+    """
+    def __init__(self, filename, google_sheet_cls, catelite_path='.'):
+        self.filename = filename
+        self.sheet_cls = google_sheet_cls
+        self.catelite_path = catelite_path
+        self.image_clses = self.get_images(self.sheet_cls, self.catelite_path)
+
+    def get_images(self, sheet_cls, catelite_path):
+        images_clses = []
+        for img_id in sheet_cls.content_list[:, 1]:
+            try:
+                images_clses.append(SG2Image(img_id))
+            except:
+                if img_id != "" and img_id != 'Image ID':
+                    print("Image ID '%s' is not in the right format." % img_id)
+                images_clses.append(SG2Image())
+                continue
+        for mission in sheet_cls.missions:
+            msk = np.where(sheet_cls.missions_list == mission)[0]
+            camera_dir = os.path.join(catelite_path, 'camera')
+            camera_file_path = os.path.join(camera_dir, mission)
+            camera_file_name = mission + "camera.txt"
+            camera_file = os.path.join(camera_file_path, camera_file_name)
+            camera_file_contents = np.genfromtxt(camera_file, dtype='str')
+            print i
+            for idx in msk:
+                images_clses[idx].get_camera_info(camera_file_contents)
+                # TODO Get input information. 
+        return images_clses
